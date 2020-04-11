d5e87f1c3ab5a8ab09495f3b0c432d15fd6ff563
mtsealove <40752097+mtsealove@users.noreply.github.com>
2020-04-11 20:09:16 +0900

    lot

--- main.js
+++ main.js
@@ -31,13 +31,144 @@ app.use(cookie_parser());
 
 // index page
 app.get('/', (req, res) => {
-    res.render('index');
+    const user=getUser(req); 
+    sql.getNewitems((news)=>{
+        res.render('index', {user:user, news:news});
+    });
+});
+
+app.get('/Item/:ID', (req, res)=>{
+    const ID=req.params.ID;
+    const user=getUser(req);
+
+    sql.getItem(ID, (item)=>{
+        sql.getOwnersOtherItem(ID, (other)=>{
+            sql.getQuestionList(ID, (question)=>{
+                sql.getReviewList(ID, (review)=>{
+                    var arr=req.cookies.recent;
+                    if(!arr) {
+                        arr=[];
+                    }
+                    var ap=true;
+                    for(var i=1; i<3; i++)  {
+                        if(arr[arr.length-i]&&arr[arr.length-i].id==ID) {
+                            ap=false;
+                        }
+                    }
+                    if(ap) {
+                        arr.push({id: ID, img:item.File1});
+                    }
+                    res.cookie('recent', arr);
+                    res.render('item', {user:user, item:item, other:other, question:question, review:review});
+                });
+            });
+        });
+    });
+});
+
+app.get('/Cart', (req, res)=>{
+    const user=getUser(req);
+    if(user.userCat==2) {
+        sql.getCart(user.userID, (cart)=>{
+            res.render('cart', {user:user, cart:cart});
+        });
+    } else {
+        noPermission(res);
+    }
+});
+
+app.get('/Order', (req, res)=>{
+    const user=getUser(req);
+    const item=req.query.item;
+    const qty=req.query.qty;
+    var items=[];
+    const itmeArr=item.split(',');
+    const qtyArr=qty.split(',');
+    for(var i=0; i<itmeArr.length; i++) {
+        items.push({
+            id:itmeArr[i],
+            qty: qtyArr[i]
+        });
+    }
+    if(user.userCat==2) {
+        sql.getListedItem(item, (itemRs)=>{
+            for(var i=0; i<items.length; i++) {
+                for(var j=0; j<itemRs.length; j++) {
+                    if(items[i].id==itemRs[j].ID) {
+                        const SaleQty=items[i].qty;
+                        items[i].item=itemRs[j];
+                    }
+                }
+            }
+            // console.log(items);
+            sql.getMy(user.userID, (my)=>{
+                res.render('order', {user:user, items:items, my:my[0]});
+            });
+        });
+    } else {
+        noPermission(res);
+    }
+});
+
+app.post('/ajax/Create/Order', (req, res)=>{
+    console.log(req.body);
+    const reward=req.body['reward'];
+    const item=req.body['item'];
+    const user=getUser(req);
+    var cnt=0;
+    var ids='';
+    for(var i=0; i<item.length; i++) {
+        ids+=item[i].id;
+        if(i!=item.length-1) {
+            ids+=',';
+        }
+        sql.createPay(user.userID, item[i].id, item[i].qty, item[i].price, (rs)=>{
+            cnt++;
+        });
+    }
+    var interval=setInterval(()=>{
+        if(cnt==item.length) {
+            clearInterval(interval);
+            sql.downReward(user.userID, reward, (rs)=>{
+                if(rs) {
+                    sql.removeCart(user.userID, ids, (removeRs)=>{
+                        if(removeRs) {
+                            res.json(ok);
+                        } else {
+                            res.json(not);
+                        }
+                    });
+                } else {
+                    res.json(not);
+                }
+            });
+        }
+    }, 100);
+});
+
+app.post('/ajax/Cart/Remove', (req, res)=>{
+    const user=getUser(req);
+    const ids=req.body['ids'];
+    var id='';
+    for(var i=0; i<ids.length; i++) {
+        id+=ids[i];
+        if(i!=ids.length-1) {
+            id+=',';
+        }
+    }
+    sql.removeCart(user.userID, id, (rs)=>{
+        if(rs) {
+            res.json(ok);
+        } else {
+            res.json(not);
+        }
+    });
 });
 
 app.get('/Login', (req, res) => {
     const user = getUser(req);
     if (!user.userID) {
-        res.render('login');
+        res.render('login', {user:user});
     } else {
         res.send(`<script>alert('이미 로그인 되어 있습니다'); location.href='/';</script>`);
     }
@@ -46,7 +177,7 @@ app.get('/Login', (req, res) => {
 app.get('/SignUp', (req, res) => {
     const user = getUser(req);
     if (!user.userID) {
-        res.render('sign_up');
+        res.render('sign_up', {user:user});
     } else {
         res.send(`<script>alert('이미 로그인 되어 있습니다'); location.href='/';</script>`);
     }
@@ -55,15 +186,22 @@ app.get('/SignUp', (req, res) => {
 app.get('/SignUp/Maker', (req, res) => {
     const user = getUser(req);
     if (!user.userID) {
-        res.render('signup_maker');
+        res.render('signup_maker', {user:user});
+    } else {
+        res.send(`<script>alert('이미 로그인 되어 있습니다'); location.href='/';</script>`);
+    }
+});
+
+app.get('/SignUp/Normal', (req, res)=>{
+    const user=getUser(req);
+    if(!user.userID) {
+        res.render('signup_normal', {user:user});
     } else {
         res.send(`<script>alert('이미 로그인 되어 있습니다'); location.href='/';</script>`);
     }
 });
 
 app.post('/SignUp/Maker', upload.single('profile'), (req, res)=>{
-    console.log(req.body);
-    console.log(req.file);
     const id=req.body['id'];
     const pw=req.body['pw'];
     const name=req.body['name'];
@@ -81,13 +219,32 @@ app.post('/SignUp/Maker', upload.single('profile'), (req, res)=>{
     if(req.file) {
         profile=req.file.filename;
     }
-    sql.createMember(id, pw, name, phone, email, my_addr, birth, gender, item, make_addr, intro, bank, bank_addr, profile, (rs)=>{
+    sql.createMakerMember(id, pw, name, phone, email, my_addr, birth, gender, item, make_addr, intro, bank, bank_addr, profile, (rs)=>{
         if(rs) {
             res.send(`<script>alert('회원가입이 완료되었습니다.');location.href='/Login';</script>`);
         } else {
             res.send(`<script>alert('오류가 발생하였습니다.');</script>`);
         }
     });
+});
+
+app.post('/SignUp/Normal', (req, res)=>{
+    const id=req.body['id'];
+    const pw=req.body['pw'];
+    const name=req.body['name'];
+    const phone=req.body['phone'];
+    const email=req.body['email'];
+    const my_addr=req.body['my_addr'];
+    const birth=req.body['birth'];
+    const gender=req.body['gender'];
+
+    sql.createNormalMember(id, pw, name, phone, email, my_addr, birth, gender, (rs)=>{
+        if(rs) {
+            res.send(`<script>alert('회원가입이 완료되었습니다.');location.href='/Login';</script>`);
+        } else {
+            res.send(`<script>alert('오류가 발생하였습니다.');</script>`);
+        }
+    }); 
 });
 
 app.get('/ajax/CheckID', (req, res) => {
@@ -124,26 +281,225 @@ app.post('/ajax/SendMail', (req, res) => {
         subject: '싸투리마켓 회원가입 인증번호입니다.',   // 제목
         text: '싸투리마켓 회원가입 인증번호는 ' + code + ' 입니다.'  // 내용
     };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
+    sql.checkMailAble(mail, (able)=>{
+        if(able) {
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    res.json({
+                        code: '-1'
+                    })
+                }
+                else {
+                    console.log('Email sent: ' + info.response);
+                    res.json({
+                        code: code
+                    });
+                }
+            });
+        } else {
             res.json({
-                code: '-1'
-            })
-        }
-        else {
-            console.log('Email sent: ' + info.response);
-            res.json({
-                code: code
+                code: '0'
             });
         }
+    });
+});
+
+app.post('/ajax/Login', (req, res)=>{
+    const id=req.body['id'];
+    const pw=req.body['pw'];
+    const cat=req.body['cat'];
+
+    sql.login(id, pw, cat, (rs)=>{
+        if(rs) {
+            req.session.userID=rs.ID;
+            req.session.userName=rs.Name;
+            req.session.userCat=rs.Cat;
+            res.json(ok);
+        } else {
+            res.json(not);
+        }
+    });
+});
+
+app.get('/Logout', (req, res)=>{
+    req.session.userID=null;
+    req.session.userCat=null;
+    req.session.userName=null;
+    res.send(`<script>alert('로그아웃 되었습니다.');location.href='/';</script>`);
+});
+
+app.get('/MyPage', (req, res)=>{
+    const user=getUser(req);
+    if(user.userID) {
+        res.render('mypage', {user:user});
+    } else {
+        noPermission(res);
+    }
+});
+
+// 생산자
+app.get('/Maker/Items', (req, res)=>{
+    const user=getUser(req);
+    if(user.userCat==1) {
+        sql.getMyItem(user.userID, (items)=>{
+            res.render('my_item', {user:user, items:items});
+        });
+        
+    } else {
+        noPermission(res);
+    }
+});
+
+app.get('/Maker/Item/Manage', (req, res)=>{
+    const user=getUser(req);
+    if(user.userCat==1) {
+        sql.getMyItem(user.userID, (items)=>{
+            res.render('my_item_manage',{user:user, items:items});
+        });
+    } else {
+        noPermission(res);
+    }
+});
+app.get('/Maker/Item/Register', (req, res)=>{
+    const user=getUser(req);
+    if(user.userCat==1) {
+        sql.getCat((cat)=>{
+            res.render('register_item', {user:user, cat: cat});
+        });
+    } else {
+        noPermission(res);
+    }
+});
+
+app.post('/Maker/Item/Register', upload.array('file', 3), (req, res)=>{
+    const name=req.body['name'];
+    const des=req.body['des'];
+    const addr=req.body['make_addr'];
+    const date=req.body['date'];
+    const price=req.body['price'];
+    const cat=req.body['cat'];
+    const unit=req.body['unit'];
+    const reqDelivery=req.body['delivery'];
+    var delivery='';
+    const pack=req.body['pack'];
+    const file1=req.files[0].filename;
+    const file2=req.files[1].filename;
+    var file3=null;
+    if(req.files[2]) {
+       file3=req.files[2].filename;
+    }
+    const user=getUser(req);
+    if(Array.isArray(reqDelivery)) {
+        for(var i=0; i<reqDelivery.length; i++) {
+            delivery+=reqDelivery[i];
+            if(i!=reqDelivery.length-1) {
+                delivery+='|';
+            }
+        }
+    } else {
+        delivery=reqDelivery;
+    }
+    sql.createItem(user.userID, name, des, addr, date, price,file1, file2, file3, cat,unit, delivery, pack, (rs)=>{
+        if(rs) {
+            res.send(`<script>alert('상품이 등록되었습니다.');location.href='/Maker/Items';</script>`);
+        } else {
+            res.send(`<script>alert('오류가 발생하였습니다.');history.go(-1);</script>`);
+        }
+    });
+});
+
+app.post('/ajax/Maker/Remove/Item', (req, res)=>{
+    const ids=req.body['ids'];
+    sql.removeItems(ids, (rs)=>{
+        if(rs) {
+            res.json(ok);
+        } else {
+            res.json(not);
+        }
+    });
+}); 
+
+app.post('/ajax/Maker/Update/Item', (req, res)=>{
+    const id=req.body['id'];
+    const price=req.body['price'];
+    const qty=req.body['qty'];
+
+    sql.updatePriceQty(id, price, qty, (rs)=>{
+        if(rs) {
+            res.json(ok);
+        } else {
+            res.json(not);
+        }
+    });
+});
+
+app.post('/ajax/Qna', (req, res)=>{
+    const title=req.body['title'];
+    const content=req.body['content'];
+    const itemID=req.body['item'];
+    const user=getUser(req);
+
+    sql.createQuestion(itemID, user.userID, title, content, (rs)=>{
+        if(rs) {
+            res.json(ok);
+        } else {
+            res.json(not);
+        }
+    });
+});
+app.post('/ajax/Review', (req, res)=>{
+    const title=req.body['title'];
+    const content=req.body['content'];
+    const itemID=req.body['item'];
+    const user=getUser(req);
+
+    sql.createReview(itemID, user.userID, title, content, (rs)=>{
+        if(rs) {
+            res.json(ok);
+        } else {
+            res.json(not);
+        }
+    });
+});
+
+app.post('/ajax/Update/Review', (req, res)=>{
+    const id=req.body['id'];
+    sql.updateReviewClick(id, (rs)=>{
+        res.json(ok);
+    })
+});
+
+app.post('/ajax/Update/Question', (req, res)=>{
+    const id=req.body['id'];
+    sql.updateQuestionClick(id, (rs)=>{
+        res.json(ok);
+    })
+});
+
+app.get('/ajax/Get/Best', (req, res)=>{
+    const cat=req.query.cat;
+    sql.getBestItems(cat, (best)=>{
+        res.json(best);
+    });
+});
+
+app.post('/ajax/InsertCart', (req, res)=>{
+    const item=req.body['item'];
+    const qty=req.body['qty'];
+    const user=getUser(req);
+    sql.addToCart(item, qty, user.userID, (rs)=>{
+        res.json(rs);
     });
 });
 
 app.listen(80, () => {
     console.log('afa runnings');
 });
+
+function noPermission(res)  {
+    res.send(`<script>alert('접근 권한이 없습니다.');history.go(-1)</script>`);
+}
 
 function getUser(req) {
     const id = req.session.userID;
--- sql.js
+++ sql.js
@@ -23,9 +23,9 @@ exports.checkIdReuse=(id, callback)=>{
     });
 }
 const crypto=require('./crypto');
-exports.createMember=(id, pw, name, phone, email, myAddr, birth, gender, item, makeAddr, intro, bank, bank_addr, profile, callback)=>{
+exports.createMakerMember=(id, pw, name, phone, email, myAddr, birth, gender, item, makeAddr, intro, bank, bank_addr, profile, callback)=>{
     var query=`insert into Members set ID='${id}', Name='${name}', Phone='${phone}', Email='${email}', MyAddr='${myAddr}',
-     Birth='${birth}', Gender='${gender}', Items='${item}', MakeAddr='${makeAddr}', Intro='${intro}', Bank='${bank}', BankAddr='${bank_addr}', Pw='${crypto.Chipe(pw)}'`;
+     Birth='${birth}', Gender='${gender}', Items='${item}', MakeAddr='${makeAddr}', Intro='${intro}', Bank='${bank}', BankAddr='${bank_addr}', Pw='${crypto.Chipe(pw)}', Cat=1`;
      if(profile) {
          query+=` , Profile='${profile}'`;
      }
@@ -39,4 +39,371 @@ exports.createMember=(id, pw, name, phone, email, myAddr, birth, gender, item, m
         }
      });
 
+}
+
+exports.createNormalMember=(id, pw, name, phone, email, my_addr, birth, gender, callback)=>{
+    const query=`insert into Members set ID='${id}', Name='${name}', Phone='${phone}', Email='${email}', MyAddr='${my_addr}',
+    Birth='${birth}', Gender='${gender}', Pw='${crypto.Chipe(pw)}', Cat=2`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.checkMailAble=(mail, callback)=>{
+    const query=`select count(ID) cnt from Members where Email='${mail}'`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else if(rs[0].cnt==0) {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    });
+}   
+
+exports.login=(id, pw, cat, callback)=>{
+    const query=`select ID, Name, Cat from Members where ID='${id}' and Pw='${crypto.Chipe(pw)}' and Cat=${cat}`;
+    connection.query(query, (e0,rs )=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            if(rs[0]) {
+                callback(rs[0]);
+            } else {
+                callback(null);
+            }
+        }
+    });
+}
+
+exports.createItem=(Owner, Name, Des, Addr, MDate, Price, File1, File2, File3, Cat, Unit, Delivery, Pack, callback)=>{
+    var query=`insert into Item set Owner='${Owner}', Name='${Name}', Des='${Des}', Addr='${Addr}', MDate='${MDate}', 
+    Price=${Price}, File1='${File1}', File2='${File2}', Cat=${Cat}, Unit='${Unit}', Delivery='${Delivery}', Pack='${Pack}' `;
+    if(File3) {
+        query+=` ,File3='${File3}'`;
+    }
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.getMyItem=(Owner, callback)=>{
+    const query=`select * from Item where Owner='${Owner}' order by ID desc`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+exports.removeItems=(ids, callback)=>{
+    var id='';
+    for(var i=0; i<ids.length; i++) {
+        id+=ids[i];
+        if(i!=ids.length-1) {
+            id+=',';
+        }
+    }
+    const getQuery=`select File1, File2, File3 from Item where ID in(${id})`;
+    const removeQuery=`delete from Item where ID in(${id})`;
+    connection.query(getQuery, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            // 파일 삭제
+            for(var i=0; i<rs.length; i++) {
+                fs.unlinkSync(__dirname+'/public/uploads/'+rs[i].File1);
+                fs.unlinkSync(__dirname+'/public/uploads/'+rs[i].File2);
+                if(rs[i].File3) {
+                    fs.unlinkSync(__dirnames+'/public/uploads/'+rs[i].File3);
+                }
+            }
+            connection.query(removeQuery, (e1)=>{
+                if(e1) {
+                    console.error(e1);
+                    callback(false);
+                } else {
+                    callback(true);
+                }
+            });
+        }
+    });
+}
+
+exports.updatePriceQty=(id, price, qty, callback)=>{
+    const query=`update Item set Price=${price}, Qty=${qty} where ID=${id}`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    }); 
+}
+
+exports.getCat=(callback)=>{
+    const query=`select * from Cat`;
+
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            callback(null);
+            console.error(e0);
+        }  else {
+            callback(rs);
+        }
+    });
+}
+
+exports.getNewitems=(callback)=>{
+    const query=`select * from Item order by ID desc limit 4`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+
+exports.getItem=(id, callback)=>{
+    const query=`select I.*, M.Name MakerName from Item I join Members M
+    on I.Owner=M.ID where I.ID=${id}`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            if(rs[0]) {
+                callback(rs[0]);
+            } else {
+                callback(null);
+            }
+        }
+    });
+}
+
+exports.getOwnersOtherItem=(id, callback)=>{
+    const query=`select * from Item where Owner=
+    (select Owner from Item where ID=${id} limit 1)
+    order by Sale desc limit 4`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+
+exports.getQuestionList=(id, callback)=>{
+    const query=`select Q.ID, Q.Title,Q.Content, date_format(Q.CTime, '%Y-%m-%d') Date, Q.Click , M.Name from Question Q left outer join  Members M
+    on Q.MemberID=M.ID where Q.ItemID=${id}`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+
+exports.getReviewList=(id, callback)=>{
+    const query=`select Q.ID, Q.Title, Q.Content, date_format(Q.CTime, '%Y-%m-%d') Date, Q.Click , M.Name from Review Q left outer join  Members M
+    on Q.MemberID=M.ID where Q.ItemID=${id}`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+
+exports.createQuestion=(itemID, memberID, Title, Content, callback)=>{
+    const query=`insert into Question set ItemID=${itemID}, MemberID='${memberID}', Title='${Title}', Content='${Content}'`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.createReview=(itemID, memberID, Title, Content, callback)=>{
+    const query=`insert into Review set ItemID=${itemID}, MemberID='${memberID}', Title='${Title}', Content='${Content}'`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.updateReviewClick=(id, callback)=>{
+    const query=`update Review set Click=Click+1 where ID=${id}`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.updateQuestionClick=(id, callback)=>{
+    const query=`update Question set Click=Click+1 where ID=${id}`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.getBestItems=(cat, callback)=>{
+    var query=`select * from Item `;
+    if(cat!=-1) {
+        query+=` where Cat=${cat} `;
+    }
+    query+=` order by Sale desc limit 4`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+
+exports.addToCart=(item, qty, user, callback)=>{
+    var res={
+        rs:0
+    };
+    const getQuery=`select count(id) cnt from Cart where ItemID=${item} and MemberID='${user}'`;
+    connection.query(getQuery, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(res);
+        }  else {
+            if(rs[0].cnt!=0) {
+                res.rs=2;
+                callback(res);
+            } else {
+                const insertQuery=`insert into Cart set ItemID=${item}, MemberID='${user}', Qty=${qty}`;
+                connection.query(insertQuery, (e1)=>{
+                    if(e1) {
+                        console.error(e1);
+                        callback(res);
+                    } else {
+                        res.rs=1;
+                        callback(res);
+                    }
+                });
+            }
+        }
+    });
+}
+
+exports.getCart=(user, callback)=>  {
+    const query=`select I.*, C.Qty CartQty from Item I join 
+    (select ItemID, Qty from Cart where MemberID='${user}') C
+    on I.ID=C.ItemID`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    })
+}
+
+exports.removeCart=(user, id, callback)=>{
+    const query=`delete from Cart where MemberID='${user}' and ItemID in (${id})`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
+}
+
+exports.getListedItem=(ids, callback)=>{
+    const query=`select I.*, M.Bank, M.BankAddr, M.Name AcName from Item I join Members M
+    on I.Owner=M.ID where I.ID in(${ids})`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    });
+}
+
+exports.getMy=(user, callback)=>{
+    const query=`select ID, Name, Email, Phone, MyAddr, Reward from Members where ID='${user}'`;
+    connection.query(query, (e0, rs)=>{
+        if(e0) {
+            console.error(e0);
+            callback(null);
+        } else {
+            callback(rs);
+        }
+    })
+}
+
+exports.createPay=(user, ItemID, Qty, Price, callback)=>{
+    const query=`insert into Pay set MemberID='${user}', ItemID=${ItemID}, Qty=${Qty}, Price=${Price}, PayTime=now()`;
+    connection.query(query, (e0)=>{
+        if(e0 ){
+            console.error(e0);
+            callback(false);
+        }else {
+            callback(true);
+        }
+    });
+}
+
+exports.downReward=(user, reward, callback)=>{
+    const query=`update Members set Reward=Reward+${reward} where ID='${user}'`;
+    connection.query(query, (e0)=>{
+        if(e0) {
+            console.error(e0);
+            callback(false);
+        } else {
+            callback(true);
+        }
+    });
 }
